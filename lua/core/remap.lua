99d68f5f40cc1bc82bd4e4882cdd4b4009fe339a
redas24 <lchlebosz@gmail.com>
2024-01-07 05:07:01 +0000

    add new remap

--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -17,3 +17,9 @@ vim.api.nvim_set_keymap('n', '<leader>fg', [[<cmd>lua require('telescope.builtin
 vim.api.nvim_set_keymap('n', '<leader>fb', [[<cmd>lua require('telescope.builtin').buffers()<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>fh', [[<cmd>lua require('telescope.builtin').help_tags()<CR>]], { noremap = true, silent = true })
 
+-- Set up key mappings
+vim.api.nvim_set_keymap('n', '<leader>ha', '<cmd>lua require("harpoon.mark").add_file()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>hh', '<cmd>lua require("harpoon.ui").toggle_quick_menu()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>hn', '<cmd>lua require("harpoon.term").gotoTerminal(1)<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ho', '<cmd>lua require("harpoon.term").gotoTerminal(2)<CR>', { noremap = true, silent = true })
+
